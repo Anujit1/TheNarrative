4d7dcec3cc9f6a29eb794c52a43d680c6814d207
Anujit Singh <anujit922@gmail.com>
2025-08-27 13:00:40 +0530

    feat(auth): add cookie-based auth middleware and enable cookie-parser

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ const path = require('path');
 const {PORT, MONGO_URI} = require('./config');
 const userRoute = require('./routes/user')
 const { connectMongodb } = require('./connections');
+const cookieParser = require('cookie-parser');
+const { checkForAuthenticationCookie } = require('./middleware/authentication');
 const localsMiddleware = require('./middleware/locals');
 
 // variables
 const app = express();
 connectMongodb(MONGO_URI);
 
-//middleware
-app.use(express.urlencoded({extended: false})); 
-app.use(localsMiddleware);
-
 // view engine
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
+
+//middleware
+app.use(express.urlencoded({extended: false})); 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie('token'));
+app.use(localsMiddleware);
+
 
 // route
 app.get('/', (req, res) => {
