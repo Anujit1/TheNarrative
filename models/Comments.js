a078c73d2693c52a6c3e859f5c048da8ff1544ed
Anujit Singh <anujit922@gmail.com>
2025-09-04 12:29:02 +0530

    feat(comment): add comment model

---
+++ models/Comments.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const commentSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  articleId:{
+    type: Schema.Types.ObjectId,
+    ref: 'article',
+    required: true
+  },
+  createdBy:{
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  }
+}, { timestamps: true, versionKey: false });
+
+
+const Comment = model('comment', commentSchema);
+
+module.exports = Comment;
